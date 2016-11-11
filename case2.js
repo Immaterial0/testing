27d0c595a1354ae4ffb3a0c695b1143bb942489d
Immaterial0 <Immaterial0@users.noreply.github.com>
2016-11-11 09:58:59 -0800

    Delete case2.js

--- case2.js
+++
@@ -1,9 +0,0 @@
-function case2(N) {
-  var threes = Math.floor(--N / 3);
-  var fives = Math.floor(N / 5);
-  var fifteen = Math.floor(N / 15);
-  
-  return (3 * threes * (threes + 1) + 5 * fives * (fives + 1) - 15 * fifteen * (fifteen + 1)) / 2;
-}
-
-module.exports = case2;
